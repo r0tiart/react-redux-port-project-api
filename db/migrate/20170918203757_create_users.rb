8a49b08b04d9c23e597e0a39ebb9e2dfa3ea2e8c
r0tiart <antley.chu@gmail.com>
2017-09-18 16:48:10 -0400

    added bcrypt and user model and migration

---
+++ db/migrate/20170918203757_create_users.rb
@@ -0,0 +1,11 @@
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.text :username
+      t.text :password_digest 
+      t.text :email
+
+      t.timestamps
+    end
+  end
+end
