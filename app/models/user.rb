612b552a571e196ab8d6b8a882f387b9cbf14781
r0tiart <antley.chu@gmail.com>
2017-09-18 21:53:37 -0400

    added methods to follow and unfollow

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,19 @@ class User < ApplicationRecord
   	has_many :followers, through: :passive_relationships, source: :follower
 
 	has_secure_password
+
+
+  def follow(other_user)
+    following << other_user
+  end
+
+  # Unfollows a user.
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+
+  # Returns true if the current user is following the other user.
+  def following?(other_user)
+    following.include?(other_user)
+  end
 end
