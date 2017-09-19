8d777db21ede350701fc02a95f9a3caafc4afd31
r0tiart <antley.chu@gmail.com>
2017-09-19 15:14:59 -0400

    added create and show methods in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,23 +4,26 @@ class UsersController < ApplicationController
 		render json: @user
 	end
 
-	def new
-	end
 
 	def create
+		@user = User.new(user_params)
+
+		if @user.save
+			session[:user_id] = @user.id
+			render json: @user
+		end
 	end
 
 	def show
+		@user = User.find_by(id: params[:id])
+		render json: @user
 	end
 
 	def update
 	end
 
-	def destroy
-	end
-
 private
 	def user_params
-		params.require(:user).permit(:username, :email, :password)
+		params.require(:user).permit(:username, :email, :password, :avatar)
 	end
 end
