eb0b9ff466b23e707cfc4327e14b16d6c298df65
r0tiart <antley.chu@gmail.com>
2017-09-19 15:11:07 -0400

    added index route that shows all user in .json

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,26 @@
 class UsersController < ApplicationController
+	def index
+		@user = User.all 
+		render json: @user
+	end
+
+	def new
+	end
+
+	def create
+	end
+
+	def show
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+private
+	def user_params
+		params.require(:user).permit(:username, :email, :password)
+	end
 end
